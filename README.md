2fb1946ba5be8ea829dd812cc4ea6b1659ec731e
jhosko <jhosko@hour-media.com>
2026-03-19 16:59:34 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# DBusiness Company Culture Awards
+
+Upload this project to Vercel to create a test site.
+
+Then replace the index page with the full build.
---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:40,fontFamily:"Arial"}}>
+      <h1>DBusiness Company Culture Awards</h1>
+      <p>Prototype is ready. Design and functionality will be layered next.</p>
+    </div>
+  );
+}
